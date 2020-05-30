8e18032fbc8fe5468f78bdf4d20e09f72051c2fb
Thinh Ngo <thinhntg@hybrid-technologies.vn>
2020-05-30 22:39:25 +0700

    add sign-in

--- android/app/src/main/java/com/example/multicast/MainActivity.java
+++ android/app/src/main/java/com/example/multicast/MainActivity.java
@@ -17,32 +17,9 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
-        if(user!=null){
-            startActivity(new Intent(MainActivity.this,GroupView_Activity.class));
-            finish();
-        }else{
-            setContentView(R.layout.activity_main);
-        }
+        FirebaseAuth.getInstance().signOut();
+        startActivity(new Intent(MainActivity.this,SignIn_Activity.class));
 
-//        btnSignin = (TextView)findViewById(R.id.tvSignI);
-//        btnSignUp = (TextView)findViewById(R.id.tvSignU);
-//
-//        btnSignin.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//                Intent i = new Intent(MainActivity.this, SignIn_Activity.class);
-//                startActivity(i);
-//            }
-//        });
-//
-//        btnSignUp.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//                Intent i = new Intent(MainActivity.this, SignUp_Activity.class);
-//                startActivity(i);
-//            }
-//        });
 
     }
 
